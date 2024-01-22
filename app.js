8743374581962d6f65c7e2d80d6f5b83124f1139
gabaal <geraldbaalham@live.co.uk>
2024-01-22 13:59:32 +0000

    Stretch goals complete

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
 console.log("Working");
 
-const form = document.getElementById("form");
-const imagecontainer = document.getElementById("img-container");
+const accessKey = "yatmYfJkdbM9yR9r8Icqdx7Gp_Xn3jlv1uzU5tMH8Ig";
+let query = "nature";
+const perPage = 9;
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  console.log(event);
-  let query = event.target.input.value;
-  search(query);
-});
+async function searchImages() {
+  const searchInput = document.getElementById("search-input");
+  query = searchInput.value || "nature";
 
-async function search(queryParam) {
-  let response = await fetch(
-    `https://api.unsplash.com/search/photos?page=1&query=${queryParam}&client_id=LP8sHkw66NXOgm6tfja-353whstZEnceimVQ6XSdlhw`
-  );
-  console.log(response);
-  let data = await response.json();
-  console.log(data.results[0].urls.raw);
+  const endpoint = `https://api.unsplash.com/photos/random/?query=${query}&count=${perPage}&client_id=${accessKey}`;
 
-  let img = document.createElement("img");
-  img.src = data.results[0].urls.raw;
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
 
-  imagecontainer.appendChild(img);
+    const imageContainer = document.getElementById("image-gallery");
+    imageContainer.innerHTML = "";
+
+    data.forEach((photo) => {
+      const imageElement = document.createElement("div");
+      imageElement.classList.add("image-container");
+
+      const overlay = document.createElement("div");
+      overlay.classList.add("image-overlay");
+
+      const overlayText = document.createElement("div");
+      overlayText.classList.add("overlay-text");
+      overlayText.innerText =
+        photo.alt_description || "No description available";
+
+      overlay.appendChild(overlayText);
+
+      imageElement.appendChild(overlay);
+
+      imageElement.innerHTML += `<img src="${photo.urls.regular}" alt="${photo.alt_description}">`;
+      imageContainer.appendChild(imageElement);
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 }
+
+searchImages();
