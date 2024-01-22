0aeee62086dbae96cf26f9157fe98ed57e9cc623
gabaal <geraldbaalham@live.co.uk>
2024-01-22 11:34:17 +0000

    Starter files created

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 console.log("Working");
 
 const form = document.getElementById("form");
-const imageContainer = document.getElementById("imageContainer");
+const imagecontainer = document.getElementById("img-container");
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log(event);
@@ -10,13 +11,15 @@ form.addEventListener("submit", function (event) {
 });
 
 async function search(queryParam) {
-  let reponse = await fetch(
+  let response = await fetch(
     `https://api.unsplash.com/search/photos?page=1&query=${queryParam}&client_id=LP8sHkw66NXOgm6tfja-353whstZEnceimVQ6XSdlhw`
   );
-  console.log(reponse);
-  let data = await reponse.json();
-  console.log(data.results);
+  console.log(response);
+  let data = await response.json();
+  console.log(data.results[0].urls.raw);
+
   let img = document.createElement("img");
   img.src = data.results[0].urls.raw;
-  imageContainer.appendChild(img);
+
+  imagecontainer.appendChild(img);
 }
